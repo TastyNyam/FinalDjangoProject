55c2c1918cd10a68974c6e9c5a91b69724005de8
TastyNyam <sultanbelekov020@gmail.com>
2026-03-15 14:47:34 +0600

    commit

---
+++ briefblog/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'briefblog.apps.BriefblogConfig'
--- briefblog/signals.py
+++ briefblog/signals.py
@@ -1,15 +1,19 @@
+# briefblog/signals.py
 from django.db.models.signals import post_save
-from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 from .models import Profile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    # Профиль создаем ТОЛЬКО если пользователь был только что создан
     if created:
-        Profile.objects.create(user=instance)
+        # Используем get_or_create на случай, если профиль уже
+        # успел создаться каким-то другим образом
+        Profile.objects.get_or_create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    # Используем getattr, чтобы избежать ошибки, если профиля почему-то нет
+    # Проверяем наличие профиля перед сохранением, чтобы не упасть в ошибку
     if hasattr(instance, 'profile'):
         instance.profile.save()
